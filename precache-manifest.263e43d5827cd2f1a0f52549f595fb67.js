ba03163d8e060d41c4a123b251e03a7de3ae53f5
monish@xamidea.in <monish@xamidea.in>
2020-08-13 10:13:05 +0530

    Updates

---
+++ precache-manifest.263e43d5827cd2f1a0f52549f595fb67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f368d6fbcf5aa23da7c7cc6082c9853e",
+    "url": "/github-info/index.html"
+  },
+  {
+    "revision": "6c058df7bde09aeb2841",
+    "url": "/github-info/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7fc464c3a395f44360b6",
+    "url": "/github-info/static/css/main.ded5bd13.chunk.css"
+  },
+  {
+    "revision": "6c058df7bde09aeb2841",
+    "url": "/github-info/static/js/2.0b6a0d56.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/github-info/static/js/2.0b6a0d56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fc464c3a395f44360b6",
+    "url": "/github-info/static/js/main.c7ae3e12.chunk.js"
+  },
+  {
+    "revision": "54826a3a14f03503bd6b",
+    "url": "/github-info/static/js/runtime-main.10fbeace.js"
+  }
+]);
